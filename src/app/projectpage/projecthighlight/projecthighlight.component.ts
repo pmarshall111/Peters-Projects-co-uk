c00c4f29bedc6791bb65ffe461fb3830323254bb
peter <pmarshall1993@hotmail.com>
2020-05-03 13:17:58 +0100

    added next and prev buttons to projectHighlight

--- src/app/projectpage/projecthighlight/projecthighlight.component.ts
+++ src/app/projectpage/projecthighlight/projecthighlight.component.ts
@@ -1,17 +1,35 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {ProjectModel} from '../project.model';
+import {ProjectService} from '../project.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-projecthighlight',
   templateUrl: './projecthighlight.component.html',
   styleUrls: ['./projecthighlight.component.css']
 })
-export class ProjecthighlightComponent implements OnInit {
-  @Input() project: ProjectModel
+export class ProjecthighlightComponent implements OnInit, OnDestroy {
+  @Input() project: ProjectModel;
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    this.subscription = this.projectService.selectedProject.subscribe(data => {
+      this.project = data;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  nextProblem() {
+    this.projectService.nextProject();
+  }
+
+  prevProblem() {
+    this.projectService.prevProject();
   }
 
 }
--- src/app/projectpage/projectpage.component.ts
+++ src/app/projectpage/projectpage.component.ts
@@ -1,25 +1,33 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
 import {ProjectService} from './project.service';
 import {ProjectModel} from './project.model';
 import {ProjectInfoModel} from './projectInfo.model';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-projectpage',
   templateUrl: './projectpage.component.html',
   styleUrls: ['./projectpage.component.css']
 })
-export class ProjectpageComponent implements OnInit {
+export class ProjectpageComponent implements OnInit, OnDestroy {
   projects: ProjectModel[];
   selectedProject: ProjectModel;
+  subscription: Subscription;
 
-  constructor(private projectService: ProjectService ) { }
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit(): void {
     this.projects = this.projectService.getAllProjects();
-    this.selectedProject = this.projects[0];
+    this.subscription = this.projectService.selectedProject.subscribe(data => {
+      this.selectedProject = data;
+    })
   }
 
-  onChangeProject(project: ProjectModel) {
-    this.selectedProject = project;
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onChangeProject(index: number) {
+    this.projectService.selectProject(index);
   }
 }
